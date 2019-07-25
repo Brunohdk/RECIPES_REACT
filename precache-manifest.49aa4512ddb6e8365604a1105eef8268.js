8fc0308992acd69cbd20c7521fbf9961ac9dd1b7
Brunohdk <bhideki3@gmail.com>
2019-07-25 12:00:22 -0300

    Updates

---
+++ precache-manifest.49aa4512ddb6e8365604a1105eef8268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3a65a3e333fa3da12ee758147c1cf8",
+    "url": "/recipes_react/index.html"
+  },
+  {
+    "revision": "a45b569752c4ff669b94",
+    "url": "/recipes_react/static/css/main.e141e8b1.chunk.css"
+  },
+  {
+    "revision": "936a04ecfef4b24bb65a",
+    "url": "/recipes_react/static/js/2.30391352.chunk.js"
+  },
+  {
+    "revision": "a45b569752c4ff669b94",
+    "url": "/recipes_react/static/js/main.707c9e7f.chunk.js"
+  },
+  {
+    "revision": "d72d49c205b7d90431a3",
+    "url": "/recipes_react/static/js/runtime~main.b88c66ae.js"
+  }
+]);
